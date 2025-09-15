03f600f9e51a276cc499ada2ef2d16a0e3af256f
Programmer18 <febrymuhammad80@gmail.com>
2025-09-15 11:37:18 +0700

    refactor: remove interface folder

--- contracts/Pickr.sol
+++ contracts/Pickr.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "contracts/interfaces/IPickr.sol";
+import "contracts/IPickr.sol";
 
 contract Pickr is IPickr {
     constructor() {
